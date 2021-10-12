fb4ed3d99bf6b8889aba186e2c43caac5039e9ea
Federico Grandi <fgrandi30@gmail.com>
2021-10-12 14:36:30 +0200

    chore: add homework files

---
+++ 011 11-10-2021 hw/001_suffisso.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 30
+
+/*
+  Scrivere un programma in C che acquisisce una stringa di al massimo
+  30 caratteri ed un numero n. Il programma estrae il suffisso della
+  stringa di lunghezza n salvandolo in un nuovo array e visualizza il
+  risultato. Se n è maggiore della lunghezza della stringa, il
+  suffisso sarà rappresentato dalla stringa stessa.
+*/
+
+int main() {
+  char str[MAX+1], res[MAX+1];
+  int n, len, i;
+
+  scanf("%s %d", str, &n);
+
+  len = strlen(str);
+  if (len < n)
+    n = len;
+
+  for (i = 0; i < n; i++)
+    res[i] = str[len-n+i];
+
+  res[i] = '\0';
+
+  printf("%s\n", res);
+
+  return 0;
+}
---
+++ 011 11-10-2021 hw/002_substr_palindrome.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+/*
+  Scrivere un programma in C che chiede all'utente di inserire una
+  stringa di massimo 100 caratteri. Il programma deve cercare tutte le
+  sottostringhe palindrome contenute all'interno della stringa di
+  lunghezza l che varia da 2 a n (dove n è la lunghezza della stringa
+  stessa). Per ogni diversa lunghezza l, stampare le stringhe
+  palindrome trovate ed il loro numero.
+*/
+
+int main() {
+  char str[MAX+1];
+  int n, l, pal, i, j, pal_count;
+
+  scanf("%s", str);
+
+  n = strlen(str);
+  for (l = 2; l < n; l++) {
+    printf("l: %d -> ", l);
+
+    for (i = 0, pal_count = 0; i <= n-l; i++) {
+      for (j = 0, pal = 1; j < l/2 && pal; j++) {
+        if (str[i+j] != str[i+l-j])
+          pal = 0;
+      }
+
+      if (pal) {
+        for (j = i; j <= i+l; j++)
+          printf("%c", str[j]);
+
+        printf(" ");
+        pal_count++;
+      }
+    }
+
+    printf("(%d)\n", pal_count);
+  }
+
+  return 0;
+}
