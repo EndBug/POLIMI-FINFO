df90c6ddc5eaa165b20b25ff90079798a00a4e9e
Federico Grandi <fgrandi30@gmail.com>
2021-10-15 10:33:30 +0200

    chore: update start file

--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
 wt -p Ubuntu -d "C:\GitHub\POLIMI-fondamenti-di-informatica"
 "C:\Program Files\Sublime Text\subl.exe" "C:\GitHub\POLIMI-fondamenti-di-informatica"
+explorer "C:\Users\fgran\OneDrive - Politecnico di Milano\WeBeep\082746 - FONDAMENTI DI INFORMATICA (MIELE ANTONIO ROSARIO)\Materiali"
