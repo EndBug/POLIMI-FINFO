c4afc9e8384e098657c8593d2ef6bb91bc349108
Federico Grandi <fgrandi30@gmail.com>
2021-11-04 09:25:58 +0100

    chore: fix start script

--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 wt -p Ubuntu -d "C:\GitHub\POLIMI-fondamenti-di-informatica"
 "C:\Program Files\Sublime Text\subl.exe" "C:\GitHub\POLIMI-fondamenti-di-informatica"
 explorer "C:\Users\fgran\OneDrive - Politecnico di Milano\WeBeep\082746 - FONDAMENTI DI INFORMATICA (MIELE ANTONIO ROSARIO)\Materiali"
+exit 0
