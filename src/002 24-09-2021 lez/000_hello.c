697e496b3147cc385b025848aa64c2d2f1f4ff46
Federico Grandi <fgrandi30@gmail.com>
2021-11-26 22:21:26 +0100

    fix: improve track

--- src/002 24-09-2021 lez/000_hello.c
+++ src/002 24-09-2021 lez/000_hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 /*
+  Da es. 24-09-2021
   Scrivere un programma che acquisisce un carattere da tastiera
   (diverso da un a capo) e lo visualizza. In seguito il programma
   chiede un altro carattere (diverso da un a capo) e lo visualizza.
