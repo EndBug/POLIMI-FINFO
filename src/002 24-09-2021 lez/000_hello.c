c29d1ccd98ba6298173f3d657c98a3f81ba9d341
Federico Grandi <fgrandi30@gmail.com>
2021-11-26 22:13:45 +0100

    fix: change folder type

---
+++ src/002 24-09-2021 lez/000_hello.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+  Scrivere un programma che acquisisce un carattere da tastiera
+  (diverso da un a capo) e lo visualizza. In seguito il programma
+  chiede un altro carattere (diverso da un a capo) e lo visualizza.
+*/
+
+int main() {
+  char c;
+  scanf("%c", &c);
+  printf("-> %c <-\n", c);
+
+  /*
+    Teoricamente i commenti con // non sono ammessi in C89
+    La seconda volta bisogna scartare il primo valore, perché il secondo è un
+    invio.
+  */
+  scanf(" %c", &c);
+  printf("-> %c <-\n", c);
+
+  return 0;
+}
